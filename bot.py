a2294ae854e27520ad6ba2445e5978a9cedfb8bc
Marklism <152321260+Marklism@users.noreply.github.com>
2025-12-14 00:15:53 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3422,7 +3422,7 @@ def register_ui_handlers(application):
     application.add_handler(CommandHandler("ot_monthly_report", ot_monthly_report_command))
     application.add_handler(CommandHandler("mission_monthly_report", mission_monthly_report_command))
 
-    application.add_handler(CallbackQueryHandler(ot_report_driver_callback, pattern=r"^OTR_DRIVER:")
+    application.add_handler(CallbackQueryHandler(ot_report_driver_callback, pattern=r"^OTR_DRIVER:"))
 
     application.add_handler(CallbackQueryHandler(handle_clock_button, pattern=r"^clock_(in|out)$"))
  
